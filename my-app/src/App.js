16f85ea37f435ad0ac6d001b6887c7138062e984
Dmytro Mazun <dm220m@gmail.com>
2023-08-15 10:45:13 +0200

    UC#23 Infrastructure setup

---
+++ my-app/src/App.js
@@ -0,0 +1,5 @@
+import './App.css'
+
+const App = () => <div className="App">My App</div>
+
+export default App
---
+++ my-app/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('renders My App', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/My App/i)
+  expect(linkElement).toBeInTheDocument()
+})
